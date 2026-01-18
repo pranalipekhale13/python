ba614ce4824c2592a4b2e4df388cf5229d233948
pranalipekhale13 <pekhalepranali133@gmail.com>
2026-01-18 18:21:08 +0530

    added list.py

--- list.py
+++ list.py
@@ -7,9 +7,9 @@ print(freinds[3])
 print(freinds[-2])
 print(freinds[0].title()) #same output only first letter capitallize
 '''
-'''
+
 #using individual value from a list
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 message = f"My first bicycle was a {bicycles[0].title()}."
 print(message)
-'''
+
