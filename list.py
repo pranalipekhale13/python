ab439d92c36a862c582d41dab52c7f2ec8d4ec64
pranalipekhale13 <pekhalepranali133@gmail.com>
2026-01-18 18:18:36 +0530

    added list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+'''
+freinds = ["pranali","tanuja","shruti","sarthak","abhi","akshada","shivam","prasad"]
+print(freinds)
+#Accessing Element
+print(freinds[0])
+print(freinds[3])
+print(freinds[-2])
+print(freinds[0].title()) #same output only first letter capitallize
+'''
+'''
+#using individual value from a list
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
+'''
